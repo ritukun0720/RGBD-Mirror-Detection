f32e379a2aa1e3fa86495ce38ef18e7f5a9d62fc
ritukun0720 <ritukun20@gmail.com>
2025-01-29 11:52:21 +0900

    code modification

---
+++ early_stopping.py
@@ -0,0 +1,32 @@
+import torch
+
+class EarlyStopping:
+    def __init__(self, patience=10, verbose=False, delta=0, path='checkpoint.pth'):
+        self.patience = patience
+        self.verbose = verbose
+        self.delta = delta
+        self.counter = 0
+        self.best_loss = None
+        self.early_stop = False
+        self.path = path
+
+    def __call__(self, val_loss, model):
+        if self.best_loss is None:
+            self.best_loss = val_loss
+            self.save_checkpoint(val_loss, model)
+        elif val_loss > self.best_loss - self.delta:
+            self.counter += 1
+            if self.verbose:
+                print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decreases.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.best_loss:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), self.path)
+        self.best_loss = val_loss
